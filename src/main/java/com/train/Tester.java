4637f27e9220b33e01d5a01a9ad2eec423beff02
Hope <hope031272@gmail.com>
2020-08-11 21:24:11 +0800

    homework 2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.print("Please enter number of tickets:\n");
-        Scanner scanner = new Scanner(System.in);
-        int tickets = scanner.nextInt();
-        System.out.print("How many round-trip tickets:\n");
-        int roundtickets = scanner.nextInt();
-        System.out.println("Total tickets:"+(tickets+roundtickets)+"\n"+"Round-trip:"+roundtickets+"\n"+"Total:"+(int)((tickets*1000)+(roundtickets*2000*0.9)));
+        int totaltickets=0;
+        while(totaltickets!=-1) {
+            System.out.print("Please enter number of tickets:\n");
+            Scanner scanner = new Scanner(System.in);
+            int tickets = scanner.nextInt();
+            System.out.print("How many round-trip tickets:\n");
+            int roundtickets = scanner.nextInt();
+            System.out.println("Total tickets:" + (tickets + roundtickets) + "\n" + "Round-trip:" + roundtickets + "\n" + "Total:" + (int) ((tickets * 1000) + (roundtickets * 2000 * 0.9)));
+            System.out.println("if input -1 break else keep");
+            totaltickets = scanner.nextInt();
+        }
+
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -4,16 +4,26 @@ import sun.security.krb5.internal.Ticket
 import java.util.*
 
 fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`);
-    print("Please enter number of tickets:\n");
-    var tickets = scanner.nextInt();
-    print("How many round-trip tickets:\n");
-    var roundtickets = scanner.nextInt();
+    while(true) {
+        print("input -1 break\n")
+        val scanner = Scanner(System.`in`);
+        print("Please enter number of tickets:\n");
+        var tickets = scanner.nextInt();
+        if (tickets == -1) {
+            println("thank~see u ")
+            break
+        }
+        print("How many round-trip tickets:\n");
+        var roundtickets = scanner.nextInt();
+        if (roundtickets == -1) {
+            println("thank~ see u ")
+            break
+        }
 
 
-
-    val tra = Train(tickets,roundtickets);
-    tra.print();
+        val tra = Train(tickets, roundtickets);
+        tra.print();
+    }
 }
 
 class Train(var tickets: Int, var roundtickets:Int){
